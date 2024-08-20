5e4de1d9877ed09ae1d99e7f63f864a53ea6a8a5
APIVerve Team <hello@apiverve.com>
2024-08-20 13:37:12 -0500

    Bug Fixes

--- WorldTimeAPIClient.cs
+++ WorldTimeAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, worldtimeQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, WorldTimeQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(worldtimeQueryOptions options = null)
+        public ResponseObj Execute(WorldTimeQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(worldtimeQueryOptions options)
+        private string constructURL(WorldTimeQueryOptions options)
         {
             string url = _apiEndpoint;
 
